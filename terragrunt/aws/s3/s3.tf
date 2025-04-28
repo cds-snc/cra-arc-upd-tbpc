0014975cbbac10cecdab3c1d2187a4d3a686f1b8
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2025-04-28 14:02:29 -0400

    revert: remove versioning for s3 bucket

--- terragrunt/aws/s3/s3.tf
+++ terragrunt/aws/s3/s3.tf
@@ -4,10 +4,4 @@ module "s3_bucket" {
   billing_tag_value = var.billing_tag_value # Added required argument
   bucket_name       = var.bucket_name
   tags              = var.tags
-
-  # Add any other required or desired configurations for the S3 module here
-  # For a basic bucket, defaults might be sufficient.
-  versioning = {
-    enabled = true # Enable versioning
-  }
 }
