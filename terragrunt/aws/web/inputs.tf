812c04e2fa57a31df98b9f5bac0fd1d567c00bc1
lega0208 <20374078+lega0208@users.noreply.github.com>
2025-07-10 16:11:20 -0400

    Mark Web inputs as sensitive

--- terragrunt/aws/web/inputs.tf
+++ terragrunt/aws/web/inputs.tf
@@ -1,11 +1,15 @@
 variable "apigw_endpoint_url" {
   description = "API Gateway Endpoint URL for the CRA UPD app"
   type        = string
+
+  sensitive = true
 }
 
 variable "cloudfront_waf_allowed_ips" {
   description = "List of IP addresses to allow through the CloudFront WAF"
   type        = list(string)
+
+  sensitive = true
 }
 
 variable "web_bucket_id" {
@@ -21,4 +25,6 @@ variable "web_bucket_arn" {
 variable "web_bucket_domain" {
   description = "The domain name of the web resources S3 bucket."
   type        = string
+
+  sensitive = true
 }
