20a3407711223932126a2bb92dd503195abd6d17
lega0208 <20374078+lega0208@users.noreply.github.com>
2025-09-08 17:29:08 -0400

    Remove API Gateway

--- terragrunt/aws/api_gateway/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "apigw_endpoint_domain" {
-  value       = trimprefix(aws_apigatewayv2_api.cra_upd_apigw_endpoint.api_endpoint, "https://")
-  description = "API Gateway Endpoint Domain"
-  sensitive   = true
-}
