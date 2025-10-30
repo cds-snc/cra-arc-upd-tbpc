97107f73c9691810baec9a7f2731a5c166f7fad3
lega0208 <20374078+lega0208@users.noreply.github.com>
2025-10-30 18:23:14 -0400

    feat: use mongo-parquet for views recalculation

--- ecs-tasks/update-db/run-task.sh
+++ ecs-tasks/update-db/run-task.sh
@@ -12,8 +12,42 @@ else
   echo "Update DB task completed successfully"
 fi
 
-echo "Exporting sample data from MongoDB to S3"
 cd mongo-parquet
+
+mkdir src/data
+mkdir src/.sync_temp
+mkdir src/.views_temp
+
+echo "Downloading most recent parquet files from S3"
+uv run --directory=src mongo_parquet --download-from-remote --storage=s3
+
+echo "Syncing parquet files with MongoDB"
+uv run --directory=src mongo_parquet --sync-parquet --cleanup-temp-dir
+
+if [ $? -ne 0 ]; then
+  echo "Syncing parquet files failed"
+else
+  echo "Parquet files synced successfully"
+  echo "Uploading updated parquet files to S3"
+  uv run --directory=src mongo_parquet --upload-to-remote --storage=s3
+
+  if [ $? -ne 0 ]; then
+    echo "Uploading updated parquet files failed"
+  else
+    echo "Updated parquet files uploaded successfully"
+  fi
+fi
+
+echo "Recalculating views"
+uv run --directory=src mongo_parquet --recalculate-views --cleanup-temp-dir
+
+if [ $? -ne 0 ]; then
+  echo "Recalculating views failed"
+else
+  echo "Views recalculated successfully"
+fi
+
+echo "Exporting sample data from MongoDB to S3"
 mkdir src/sample
 uv run --directory=src mongo_parquet --export-from-mongo --upload-to-remote --storage=s3 --sample
 
@@ -22,15 +56,4 @@ if [ $? -ne 0 ]; then
   exit 1
 else
   echo "Sample data exported successfully"
-fi
-
-echo "Exporting full data from MongoDB to S3"
-mkdir src/data
-uv run --directory=src mongo_parquet --export-from-mongo --upload-to-remote --storage=s3
-
-if [ $? -ne 0 ]; then
-  echo "Exporting full data failed"
-  exit 1
-else
-  echo "Full data exported successfully"
 fi
