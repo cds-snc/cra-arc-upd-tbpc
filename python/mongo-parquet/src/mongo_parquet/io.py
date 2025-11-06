c6861214d7805c18a950d51162e4c3ab02ad83df
lega0208 <20374078+lega0208@users.noreply.github.com>
2025-11-06 11:15:32 -0500

    fix: parquet sync error for partitioned collections

--- python/mongo-parquet/src/mongo_parquet/io.py
+++ python/mongo-parquet/src/mongo_parquet/io.py
@@ -243,7 +243,10 @@ class MongoParquetIO:
                             )
 
                             pl.concat(
-                                [parquet_model.lf(), new_data.lazy()]
+                                [
+                                    pl.scan_parquet(storage_filepath),
+                                    new_data.lazy(),
+                                ]
                             ).sink_parquet(
                                 temp_storage_filepath,
                                 compression_level=7,
@@ -251,11 +254,15 @@ class MongoParquetIO:
                                 sync_on_close="all",
                             )
 
+                            print(
+                                f"Replacing {storage_filepath} with temp file {temp_storage_filepath}..."
+                            )
+
                             os.replace(temp_storage_filepath, storage_filepath)
 
                             print(f"Successfully wrote to {storage_filepath}")
                         except Exception as e:
-                            error(e.add_note(f"Error writing to {storage_filepath}"))
+                            error(e)
                             sync_utils.restore_backup(filepath)
                     else:
                         print(f"Writing to {filepath}")
@@ -267,7 +274,7 @@ class MongoParquetIO:
                             )
                             print(f"Successfully wrote to {storage_filepath}")
                         except Exception as e:
-                            error(e.add_note(f"Error writing to {storage_filepath}"))
+                            error(e)
 
                     print(
                         f"Updated {filepath} in {format_timedelta(datetime.now() - partition_start_time)}"
