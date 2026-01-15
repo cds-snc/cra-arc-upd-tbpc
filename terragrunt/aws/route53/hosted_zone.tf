c8796ad3aec0e402360e6850e90408a7a6002406
lega0208 <20374078+lega0208@users.noreply.github.com>
2026-01-15 12:47:15 -0500

    infra: Set proper name servers for prod hosted zone

--- terragrunt/aws/route53/hosted_zone.tf
+++ terragrunt/aws/route53/hosted_zone.tf
@@ -1,5 +1,12 @@
 resource "aws_route53_zone" "cra_upd_hosted_zone" {
   name = var.domain
 
+  name_servers = var.env == "production" ? [
+    "ns1-06.azure-dns.com",
+    "ns2-06.azure-dns.net",
+    "ns3-06.azure-dns.org",
+    "ns4-06.azure-dns.info",
+  ] : null
+
   depends_on = [aws_acm_certificate.cra_upd_cloudfront_acm]
 }
