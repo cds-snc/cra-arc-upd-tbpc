03d1ff48494991e7d82b7ff584eaf99eb225c953
lega0208 <20374078+lega0208@users.noreply.github.com>
2026-04-22 09:18:10 -0400

    fix(mongo-parquet): Add safeguards to prevent issues if view date ranges are duplicated

--- python/mongo-parquet/src/mongo_parquet/views/utils.py
+++ python/mongo-parquet/src/mongo_parquet/views/utils.py
@@ -1,7 +1,8 @@
 import os
 import polars as pl
 import re
-from datetime import timedelta
+from datetime import datetime, timedelta
+from .daterange_utils import DateRange
 
 
 class ViewsUtils:
@@ -9,6 +10,8 @@ class ViewsUtils:
         temp_dir_str = os.path.join(parquet_dir_path, "..", temp_dir_name)
         self.parquet_dir_path: str = os.path.abspath(parquet_dir_path)
         self.temp_dir_path: str = os.path.abspath(temp_dir_str)
+        self._already_calculated_views: set[tuple[datetime, datetime]] = set()
+        self._already_inserted_views: set[tuple[datetime, datetime]] = set()
 
     def ensure_temp_dir(self):
         if not os.path.exists(self.temp_dir_path):
@@ -35,6 +38,24 @@ class ViewsUtils:
         self.ensure_temp_dir()
         temp_file_path = os.path.join(self.temp_dir_path, file_name)
         lf.sink_parquet(temp_file_path, compression_level=5, engine="streaming")
+
+    def set_view_calculated(self, date_range: DateRange):
+        self._already_calculated_views.add((date_range["start"], date_range["end"]))
+
+    def is_view_calculated(self, date_range: DateRange) -> bool:
+        return (date_range["start"], date_range["end"]) in self._already_calculated_views
+
+    def clear_already_calculated_views(self):
+        self._already_calculated_views.clear()
+
+    def set_view_inserted(self, date_range: DateRange):
+        self._already_inserted_views.add((date_range["start"], date_range["end"]))
+
+    def is_view_inserted(self, date_range: DateRange) -> bool:
+        return (date_range["start"], date_range["end"]) in self._already_inserted_views
+
+    def clear_already_inserted_views(self):
+        self._already_inserted_views.clear()
 
 
 def format_timedelta(td: timedelta) -> str:
