9fcbb7cbb312e5ee1f49f46673a5f88d02d6b38d
anthony959-dev <82117989+anthony959-dev@users.noreply.github.com>
2026-07-20 10:01:29 -0400

    if after 4 days and is_404 is true then is_archive is false

--- libs/db-update/src/lib/urls/urls.service.ts
+++ libs/db-update/src/lib/urls/urls.service.ts
@@ -157,7 +157,7 @@ export class UrlsService {
               },
               {
                 is_404: true,
-                is_archive: false;
+                is_archive: false,
                 last_checked: { $lt: fourDaysAgo },
               },
             ],
